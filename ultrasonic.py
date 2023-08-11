ce318767c1e379b2470b4c97003e4c4620e8caeb
OffensiveNut <hafefun7377@gmail.com>
2023-08-11 15:49:08 +0700

    update script agar support 2 sensor

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,35 +1,67 @@
 import RPi.GPIO as GPIO
 import time
+import threading
  
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
  
 #set GPIO Pins
-GPIO_TRIGGER = 15
-GPIO_ECHO = 18
+GPIO_TRIGGER1 = 18
+GPIO_ECHO1 = 15
+GPIO_TRIGGER2 = 24
+GPIO_ECHO2 = 23
 
  
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER1, GPIO.OUT)
+GPIO.setup(GPIO_ECHO1, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER2, GPIO.OUT)
+GPIO.setup(GPIO_ECHO2, GPIO.IN)
  
-def distance():
+def distance1():
     # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    GPIO.output(GPIO_TRIGGER1, True)
  
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(GPIO_TRIGGER1, False)
  
     StartTime = time.time()
     StopTime = time.time()
  
     # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
+    while GPIO.input(GPIO_ECHO1) == 0:
         StartTime = time.time()
  
     # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
+    while GPIO.input(GPIO_ECHO1) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+
+def distance2():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER2, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER2, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO2) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO2) == 1:
         StopTime = time.time()
  
     # time difference between start and arrival
@@ -43,9 +75,11 @@ def distance():
 if __name__ == '__main__':
     print('sukses')
     try:
+        print('masuk while loop')
         while True:
-            dist = distance()
-            print ("Jarak = %.1f cm" % dist)
+            dist1 = distance1()
+            dist2 = distance2()
+            print ("Jarak sensor 1 = %.1f cm" % dist1,"sensor 2 = %.1f cm" % dist2)
             time.sleep(1)
  
         # Reset by pressing CTRL + C
